e1865378da21cb7c360cd016c7ee90898d72557d
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-06-25 23:49:27 -0300

    27. Utilizando o Postman

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/teste',function(Request $request) {
+	return 'Hello World';
+});
+
+Route::post('/teste',function(Request $request) {
+	return $request->all();
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
