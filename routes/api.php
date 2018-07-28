5eba457fdfb8c7a1e67f6b80d6b9ae7019d1099c
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-07-28 16:06:15 -0300

    55. Modelo de como Adicionar Conteúdos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Conteudo;
+use App\Comentario;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,3 +18,17 @@ Route::post('/cadastro','UsersController@cadastro');
 Route::post('/login','UsersController@login');
 Route::middleware('auth:api')->get('/user','UsersController@user');
 Route::middleware('auth:api')->put('/perfil','UsersController@perfil');
+
+Route::get('/testes',function(){
+    $user = User::find(1);
+    /*
+    $user->conteudos()->create([
+        'titulo' => 'Conteudo 3',
+        'texto' => 'Aqui tem um texto',
+        'imagem' => 'url da imagem',
+        'link' => 'Link',
+        'data' => '2018-07-10',
+    ]);
+    return $user->conteudos;
+    */
+});
