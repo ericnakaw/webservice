ec3d1172ce03e6982e539e3d1db8f62849e5949c
Eric Nakawatase <e.nakawatase@coblue.com.br>
2018-07-28 15:40:01 -0300

    54. Definindo os relacionamentos

--- app/Comentario.php
+++ app/Comentario.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'conteudo_id',
+        'texto',
+        'data',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    
+    public function conteudo()
+    {
+        return $this->belongsTo('App\Conteudo');
+    }
 }
